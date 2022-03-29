0e3ae1c235b37803fd8cfb2b2c6cc183bc2c11c3
sergio <daw202307@estudiantes.salesianasnsp.es>
2022-03-29 08:44:24 +0200

    Practica12

---
+++ Practica_12_GITHUB/src/practica_12_github/Practica_12_GITHUB.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica_12_github;
+
+/**
+ *
+ * @author administrador
+ */
+public class Practica_12_GITHUB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String hola = null;
+        System.out.println(hola);
+        System.out.println("Esto es un texto de prueba para la práctica 12 de GITHUB de Entornos");
+    }   
+    
+   
+    
+}
